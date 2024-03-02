ba1ed8b647ec556f94480a66f0f58b6c112fb2d1
levi yourchuck <levi.yourchuck@gmail.com>
2024-03-02 17:54:35 -0600

    LANTERNA-491--2024 conforming SplitPanel to interim changes since this 4 year old branch was started

--- src/main/java/com/googlecode/lanterna/gui2/ScrollableBox.java
+++ src/main/java/com/googlecode/lanterna/gui2/ScrollableBox.java
@@ -18,10 +18,6 @@
  */
 package com.googlecode.lanterna.gui2;
 
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.graphics.Theme;
-import com.googlecode.lanterna.graphics.ThemeDefinition;
 
 /**
  * 
