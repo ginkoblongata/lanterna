b4464dbcdb48b8fe6641cc52c08491fc389f2068
levi yourchuck <levi.yourchuck@gmail.com>
2024-03-02 18:08:21 -0600

    LANTERNA-491--2024 conforming SplitPanel to interim changes since this 4 year old branch was started

--- src/test/java/com/googlecode/lanterna/gui2/WindowManagerTest.java
+++ src/test/java/com/googlecode/lanterna/gui2/WindowManagerTest.java
@@ -7,7 +7,6 @@ import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
-import java.util.*;
 
 public class WindowManagerTest extends TestBase {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -18,8 +17,6 @@ public class WindowManagerTest extends TestBase {
     protected MultiWindowTextGUI createTextGUI(Screen screen) {
         return new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen, new CustomWindowManager());
     }
-
-    Set<String> HACK_what_are_these_orderings = new HashSet<>();
 
     @Override
     public void init(WindowBasedTextGUI textGUI) {
@@ -37,42 +34,16 @@ public class WindowManagerTest extends TestBase {
         textGUI.addWindow(mainWindow);
     }
 
-    private class CustomWindowManager extends DefaultWindowManager {
-        // not sure how to determine padding decoration
-        final int PAD = 8;
-        int xOffset;
-        @Override
-        public void prepareWindows(WindowBasedTextGUI textGUI, List<Window> allWindows, TerminalSize screenSize) {
-            trackTheOrderings(allWindows);
-            xOffset = 0;
-            super.prepareWindows(textGUI, allWindows, screenSize);
-        }
+    private static class CustomWindowManager extends DefaultWindowManager {
         @Override
         protected void prepareWindow(TerminalSize screenSize, Window window) {
             super.prepareWindow(screenSize, window);
 
             window.setDecoratedSize(window.getPreferredSize().withRelative(12, 10));
             window.setPosition(new TerminalPosition(
-                    screenSize.getColumns() - window.getDecoratedSize().getColumns() - 1 - xOffset - (2*PAD),
+                    screenSize.getColumns() - window.getDecoratedSize().getColumns() - 1,
                     screenSize.getRows() - window.getDecoratedSize().getRows() - 1
             ));
-            
-            xOffset += window.getDecoratedSize().getColumns();
-        }
-    }
-
-    void trackTheOrderings(List<Window> windows) {
-        String token = "";
-        for (Window w : windows) {
-            final int systemHashCode = System.identityHashCode(w);
-            token += systemHashCode + ", ";
-        }
-        token = "[" + token.substring(0, token.length() -2) + "]";
-        HACK_what_are_these_orderings.add(token);
-        
-        if (HACK_what_are_these_orderings.size() > 1) {
-            // put debugger break point here
-            int hmmm_not_sure_what_is_going_on = 9876;
         }
     }
 }
