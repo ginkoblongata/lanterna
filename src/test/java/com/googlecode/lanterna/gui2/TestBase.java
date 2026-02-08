7d6d27434bb541e113814dda96036631738313e8
ginkoblongata <3180783+ginkoblongata@users.noreply.github.com>
2026-02-08 04:56:36 -0600

    ScrollPanel & ScrollableBox (adapter for AbstractListBox --> ActionListBox, CheckBoxList, RadioBoxList, TextBox)

--- src/test/java/com/googlecode/lanterna/gui2/TestBase.java
+++ src/test/java/com/googlecode/lanterna/gui2/TestBase.java
@@ -14,36 +14,40 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2010-2024 Martin Berglund
+ * Copyright (C) 2010-2026 Martin Berglund
  */
 package com.googlecode.lanterna.gui2;
 
-import com.googlecode.lanterna.TestTerminalFactory;
-import com.googlecode.lanterna.bundle.LanternaThemes;
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.*;
+import com.googlecode.lanterna.bundle.*;
 import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  * Some common code for the GUI tests to get a text system up and running on a separate thread
  * @author Martin
  */
 public abstract class TestBase {
+
+    MultiWindowTextGUI textGUI;
+
     void run(String[] args) throws IOException, InterruptedException {
         Screen screen = new TestTerminalFactory(args).createScreen();
         screen.startScreen();
-        MultiWindowTextGUI textGUI = createTextGUI(screen);
-        String theme = extractTheme(args);
-        if(theme != null) {
-            textGUI.setTheme(LanternaThemes.getRegisteredTheme(theme));
-        }
+        textGUI = createTextGUI(screen);
+        assignTheme(extractTheme(args));
         textGUI.setBlockingIO(false);
         textGUI.setEOFWhenNoWindows(true);
         //noinspection ResultOfMethodCallIgnored
         textGUI.isEOFWhenNoWindows();   //No meaning, just to silence IntelliJ:s "is never used" alert
 
         try {
+            textGUI.addWindow(makeThemeChangerWindow());
             init(textGUI);
+            arrangeWindows();
             AsynchronousTextGUIThread guiThread = (AsynchronousTextGUIThread)textGUI.getGUIThread();
             guiThread.start();
             afterGUIThreadStarted(textGUI);
@@ -64,11 +68,57 @@ public abstract class TestBase {
     }
 
     protected MultiWindowTextGUI createTextGUI(Screen screen) {
-        return new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen);
+        return new MultiWindowTextGUI(new SeparateTextGUIThread.Factory(), screen, new DefaultWindowManager());
     }
 
     public abstract void init(WindowBasedTextGUI textGUI);
     public void afterGUIThreadStarted(WindowBasedTextGUI textGUI) {
         // By default do nothing
+    }
+    public Window makeThemeChangerWindow() {
+		    final Window window = new BasicWindow("Themes");
+        ActionListBox themes = new ActionListBox();
+        themes.setPreferredSize(new TerminalSize(30, 16));
+        themes.addItem( "0, theme: default        ", () -> assignTheme("default"));
+        themes.addItem( "1, theme: defrost        ", () -> assignTheme("defrost"));
+        themes.addItem( "2, theme: bigsnake       ", () -> assignTheme("bigsnake"));
+        themes.addItem( "3, theme: conqueror      ", () -> assignTheme("conqueror"));
+        themes.addItem( "4, theme: businessmachine", () -> assignTheme("businessmachine"));
+        themes.addItem( "5, theme: blaster        ", () -> assignTheme("blaster"));
+        
+        window.setComponent(themes);
+        
+        // 
+        // unsure why, there is still some flicker case if ScrollPanel not quite used preferred size 
+        //ScrollPanel scrollPanel = new ScrollPanel(themes);
+        //scrollPanel.setPreferredSize(new TerminalSize(40, 20));
+        //window.setComponent(scrollPanel);
+        return window;
+    }
+    
+    public void assignTheme(String themeName) {
+        if (themeName == null) {
+            return;
+        }
+        Theme theme = LanternaThemes.getRegisteredTheme(themeName);
+        Collection<Window> windows = textGUI.getWindows();
+        if (theme != null && windows != null) {
+            for (Window w : windows) {
+                w.setTheme(theme);
+            }
+            textGUI.setTheme(theme);
+        }
+    }
+    
+    public void arrangeWindows() {
+        final int PAD = 8;
+        int x = 1;
+        int y = 1;
+        for (Window w : textGUI.getWindows()) {
+            TerminalSize size = w.getPreferredSize();
+            w.setPosition(new TerminalPosition(x, y));
+            w.setHints(Collections.singletonList(Window.Hint.FIXED_POSITION));
+            x += size.getColumns() + PAD;
+        }
     }
 }
